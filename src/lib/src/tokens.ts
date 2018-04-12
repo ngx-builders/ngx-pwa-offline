b53599508fff1122790437a97dbd4c92032bb87c
Cyrille Tuzi <web@cyrilletuzi.com>
2018-04-12 21:48:10 +0200

    fix: wrong type for token

--- src/lib/src/tokens.ts
+++ src/lib/src/tokens.ts
@@ -2,4 +2,4 @@ import { InjectionToken } from '@angular/core';
 
 export const OFFLINE_CONFIG_ROUTE_OFFLINE = new InjectionToken<string>('offline-config-route-offline');
 export const OFFLINE_CONFIG_ROUTE_UNAVAILABLE = new InjectionToken<string>('offline-config-route-unavailable');
-export const OFFLINE_CONFIG_GUARDS_REDIRECT = new InjectionToken<string>('offline-config-guards-redirect');
+export const OFFLINE_CONFIG_GUARDS_REDIRECT = new InjectionToken<boolean>('offline-config-guards-redirect');
