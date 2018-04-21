66cfa6c1860acb580badddc094ea13d6604bf163
Cyrille Tuzi <web@cyrilletuzi.com>
2018-04-21 10:15:39 +0200

    fix: correctly detect custom config

--- src/lib/src/tokens.ts
+++ src/lib/src/tokens.ts
@@ -28,9 +28,9 @@ export interface OfflineProvidersConfig {
 export function offlineProviders(config: OfflineProvidersConfig): Provider[] {
 
   return [
-    config.routeOffline ? { provide: OFFLINE_ROUTE_OFFLINE, useValue: config.routeOffline }  : [],
-    config.routeUnavailable ? { provide: OFFLINE_ROUTE_UNAVAILABLE, useValue: config.routeUnavailable } : [],
-    config.guardsRedirect ? { provide: OFFLINE_GUARDS_REDIRECT, useValue: config.guardsRedirect } : [],
+    'routeOffline' in config ? { provide: OFFLINE_ROUTE_OFFLINE, useValue: config.routeOffline }  : [],
+    'routeUnavailable' in config ? { provide: OFFLINE_ROUTE_UNAVAILABLE, useValue: config.routeUnavailable } : [],
+    'guardsRedirect' in config ? { provide: OFFLINE_GUARDS_REDIRECT, useValue: config.guardsRedirect } : [],
   ];
 
 }
