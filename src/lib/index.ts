4828ef16f04083b986b0916cbcb7b0b73b3417aa
Cyrille Tuzi <web@cyrilletuzi.com>
2018-07-23 16:51:01 +0200

    feat: CLI project

--- src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-export {
-  OfflineProvidersConfig, offlineProviders,
-  OFFLINE_GUARDS_REDIRECT, OFFLINE_ROUTE_OFFLINE, OFFLINE_ROUTE_UNAVAILABLE
-} from './src/tokens';
-export { Network, catchOffline } from './src/network.service';
-export { OnlineGuard } from './src/online.guard';
